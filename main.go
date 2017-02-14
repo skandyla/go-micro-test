921fd7238fc15480d7cc6d73c0829a378e89ff91
Sergey Kandyla <skandyla@gmail.com>
2017-02-14 10:55:27 +0300

    use more handlers

--- main.go
+++ main.go
@@ -6,15 +6,31 @@ import (
 	"log"
 	"net/http"
 	"os"
+    "fmt"
+	"net"
 )
+
+func handlerRoot(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+func handlerTest(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"hello\": \"world\"}"))
+}
+func handlerLogs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+    fmt.Fprintf(w, "IP: %s", ip)
+}
 
 func main() {
 	middleware := stats.New()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{\"hello\": \"world\"}"))
-	})
+
+    mux.HandleFunc("/", handlerRoot)
+    mux.HandleFunc("/test", handlerTest)
+    mux.HandleFunc("/logs", handlerLogs)
+
 	mux.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		b, _ := json.Marshal(middleware.Data())
